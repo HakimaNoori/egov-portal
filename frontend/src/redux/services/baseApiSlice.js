d41c8a38cb456d2d0e05cc0bc8f2f17606e16ad3
HakimaNoori <hakima.noori.1379@gmail.com>
2025-10-08 12:28:31 +0430

    add enviroment variable

--- frontend/src/redux/services/baseApiSlice.js
+++ frontend/src/redux/services/baseApiSlice.js
@@ -16,7 +16,7 @@ export const tagTypes = {
 export const baseApiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: API_URL,
+    baseUrl: `${API_URL}/api`,
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) headers.set("authorization", `Bearer ${token}`);
